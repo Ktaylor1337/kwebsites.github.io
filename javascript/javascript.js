fafcf651df7d8d1fd8962eb842945a7d3ce8add5
Ktaylor1337 <katetaylor5155@gmail.com>
2017-06-07 21:11:52 +0100

    x

---
+++ javascript/javascript.js
@@ -0,0 +1,22 @@
+var w = document.getElementById('wrapper');
+var button = document.getElementById('randomize');
+var quotes = w.children; // inner elements, your quotes divs
+
+// a function to hide all divs
+var hideDivs = function(divs) {
+  for (var div of divs) {
+    div.style.display = 'none';
+  }
+}
+
+hideDivs(quotes); // hide all initially
+
+// on click
+button.addEventListener('click', function(event) {
+  var rnd = Math.floor(Math.random() * quotes.length); // get random index
+  hideDivs(quotes); // hide all quotes
+  quotes[rnd].style.display = 'block'; // show random quote
+  event.target.textContent = 'Generate another philosopher'; // set button text. event.target is the button you've clicked
+})
+
+
